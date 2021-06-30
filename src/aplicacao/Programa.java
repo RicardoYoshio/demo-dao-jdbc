f64b4c931ae073c439001f657a95687a685801ff
RicardoYoshio <rica.tomokane@gmail.com>
2021-06-29 21:15:46 -0300

    Departamento e entidade class

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import entidade.negocio.Departamento;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+	
+	Departamento obj = new Departamento(1, "Livro");
+	System.out.println(obj);
+	}
+}
